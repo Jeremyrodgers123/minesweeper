a375a15d9c61cb3c51f3d4e78cce7f9367591d5c
Jeremy <jeremy.rodgers88@gmail.com>
2017-07-25 21:16:07 -0400

    Recursive funcition and objects finished

--- assets/main.js
+++ assets/main.js
@@ -2,11 +2,13 @@
 $(document).ready(function(){
   $('.board').on("click", 'div', function(event){
     //collect the target event and pass it to the function
-    $selection = $(event.target)
+    $selection = $(event.target);
     event.stopPropagation();
     //console.log($selection.parent().index())
-    board.uncoverableSquares($selection)
-    game.turn();
+    var x = $selection.parent().index();
+    var y = $selection.index();
+    console.log(x,y);
+    board.checkSquare(x,y);
   })
 });
 
@@ -15,205 +17,146 @@ $(document).ready(function(){
 //[[{value: 0, 1, 2, 3, 4, isBomb: false or true, beenChecked: true or false, markedAsSafe: true or false }],[],[]]
 //board object
   var board = {
+    
   //indicates how many rows and columns the game should have
   	numRows : 10,
-    bombs : [],
-    //classes that are either covered or uncovered
-    covered: [],
+    tiles : [],
     //initalizes the boardValues Array
-    initVals : function(numRows){
-      array = [];  
-          
-      for (var j = 0; j < numRows; j++){
+    initVals : function(){
+      for (var j = 0; j < board.numRows; j++){
           newArray = [];        
-          array.push(newArray);         
-        for (var i = 0; i < numRows; i++){
+          board.tiles.push(newArray);         
+        for (var i = 0; i < board.numRows; i++){
           newArray.push({value:0, isCovered: true, isBomb: false, safe: false});
         }
       }
 
-      this.values = array
-      this.covered = arrayCovered
     },
-    values : [],
   //renders the board
     render : function(){
-      this.initVals(this.numRows);
-      this.createRows(this.numRows);
+      this.initVals();
+      this.createRows();
+      this.bombSet();
     },
   //creates rows and columns of the board
-    createRows: function(numRows){
-      for(var a = 0; a < numRows; a++){
-        $(".board").append("<div class = 'boardRow" + a + " wip' ></div>")
-        for (var i = 0; i < numRows; i++){
-          $(".boardRow" + a).append("<div class = 'block "+board.covered[a][i] +" wip' >"+ board.values[a][i] + "</div>")
+    createRows: function(){
+      for(var x = 0; x < board.numRows; x++){
+        $(".board").append("<div class = 'boardRow" + x + " wip' ></div>")
+        for (var y = 0; y < board.numRows; y++){
+          //<div id="01 or 11 or 99"
+          $(".boardRow" + x).append("<div id='"+x+""+y+"' class = 'block "+(board.tiles[x][y].isCovered ? 'covered' : '') +" wip' >"+ board.tiles[x][y].value + "</div>");
         }
      }
     },
 
-    bombSet : function(numRows){
-      for(var i = 0; i < numRows; i++){
-        var num1 = Math.floor(Math.random()*10);
-        var num2 = Math.floor(Math.random()*10);
-        var bombs = [num1,num2]
-        this.bombs.push(bombs)
-        this.values[num1][num2] = "B"
+    bombSet : function(){
+      for(var i = 0; i < board.numRows; i++){
+        var x = Math.floor(Math.random()*10);
+        var y = Math.floor(Math.random()*10);
+        //check if the tile is already a bomb if it is i-- and contiue;
+        if(board.tiles[x][y].isBomb){
+          i--;
+          continue;
+        }
+        board.tiles[x][y].isBomb = true;
+        board.tiles[x][y].value = "B" ;
+        board.updateTileHTML(x,y); 
 
-       $row = $('[class^="boardRow"]').eq(num1);
-       $block = $row.children().eq(num2);
+        board.increaseBombCount(x, y-1);
+          board.increaseBombCount(x-1, y-1);
+          board.increaseBombCount(x-1, y);
+          board.increaseBombCount(x-1, y+1);
+          board.increaseBombCount(x, y+1);
+          board.increaseBombCount(x+1, y+1);
+          board.increaseBombCount(x+1, y);
+          board.increaseBombCount(x+1, y-1);
+
+       $row = $('[class^="boardRow"]').eq(x);
+       $block = $row.children().eq(y);
        $block.addClass('bomb')
       }
-      return this.values
     },
 
-    update: function(){
-      $('.bomb').each( function(){
-        $(this).html("B")
-        });
+
+    increaseBombCount: function(x,y){
+        if(x < 0 || y < 0 || y > board.numRows -1 || x > board.numRows -1) {return;}
+        var tile = board.tiles[x][y];
+        if(tile.isBomb) {return;}
+        tile.value ++;  
+        board.updateTileHTML(x,y);  
     },
 
-    bombCheck: function(){
-      var bombSpots = this.bombs
-      for (var pair in bombSpots){
-        var x = bombSpots[pair][0];
-        var y = bombSpots[pair][1];
-        this.countMaster(x,y);
+
+
+
+
+
+//{value: {isBomb : true}}
+    checkSquare: function(x, y){
+
+      if(x < 0 || y < 0 || y > board.numRows -1 || x > board.numRows -1){
+        return;
       }
-      console.log(board.values);
 
-    },
+        //see if the square is uncovered or already checked?
+        var tile = board.tiles[x][y];
+       if(tile.isCovered == false){
+        //ignore and return
+          return;
+       }
 
-    countDownRight : function(x,y){
-      var a = x + 1
-      var b = y + 1
-      if (a < 10 && b < 10){
-        var space = this.values[a][b]
-        if (a > 0 && space !== "Bomb" ) {
-          board.values[a][b] += 1;
-        }
-      }
-    },
-
-    countMaster: function(x,y){
-      var left         = [x     , y - 1]
-      var upLeft       = [x - 1 , y - 1]
-      var up           = [x - 1 , y    ]
-      var upRight      = [x - 1 , y + 1]
-      var right        = [x     , y + 1]
-      var downRight    = [x + 1 , y + 1]
-      var down         = [x + 1 , y    ]
-      var downLeft     = [x + 1 , y - 1]
-      var directions   = [left, upLeft, up, upRight, right, downRight,down, downLeft]
-
-      for (var coordinates in directions) {
-        a = directions[coordinates][0] 
-        b = directions[coordinates][1]
-
-        if (a >= 0 && a < 10 && b >=0 && b < 10){
-           var space = this.values[a][b]
-           if (space !== "B"){
-            board.values[a][b] += 1;
-           }
-        }
-      }
-    },
-    turnRender: function(){
-
-    },
-    destroy: function(){
-      $('.board').html("");
-      console.log("is it working")
-    },
-
-    checkSquare: function()
-
-    uncoverableSquares: function(clickedSquare){
-      var x = clickedSquare.parent().index()
-      var y = clickedSquare.index()
-      var currentSpace = [x,y]
-      //console.log(x, y)
-      //find all the 0's in the space
-      var findFarLeft = function(next, complete, x, y){
-        //var complete  =[[0,0]]
-        var neighbors = board.findNeighbors(x,y)
-         //removes negative spaces from array
-        for (var array in neighbors) {
-          a = neighbors[array][0] 
-          b = neighbors[array][1]
-         if (a >= 0 && a < 10 && b >=0 && b < 10){
-           next.push(neighbors[array])
-         }
-        }
        
-        console.log(next)
-
-        for (var index in next){
-          for(var box in complete){
-            //console.log("complete box")
-            //console.log(complete[box])
-            if (next[index][0] === complete[box][0] && next[index][1] === complete[box][1]){
-              //console.log("delete me")
-              //console.log('next index')
-              //console.log(next)
-              next.splice(index,1)
-              index--
-              complete.push([x,y])
-              //console.log(next)
-              //console.log("complete")
-              //console.log(complete)
-
-            }
-          }
+        //if its a bomb the game is over uncover the whole board
+        if (tile.isBomb === true){
+          //TODO: call end game;
+          return;
         }
-        
-        console.log("***STATS*****")
-        console.log("next")
-        console.log(next)
-        console.log("complete")
-        console.log(complete)
-         console.log("next up")
-        console.log(next[0])
-      }
-      var next =[]
-      var complete =[[0,0]]
-      findFarLeft(next, complete, x, y);
+        //else if it's value is greater then 0, uncover just this square
+         if (tile.value > 0){
+          tile.isCovered = false;
+          this.updateTileHTML(x,y) 
+          return
+         }
+
+        //else it's value is 0
+        if (tile.value === 0) {
+          tile.isCovered = false;
+          //call an uncover function to show the html 
+          this.updateTileHTML(x,y)
+          board.checkSquare(x , y-1);
+          board.checkSquare(x-1, y-1);
+          board.checkSquare(x-1, y);
+          board.checkSquare(x-1, y+1);
+          board.checkSquare(x, y+1);
+          board.checkSquare(x+1, y+1);
+          board.checkSquare(x+1, y);
+          board.checkSquare(x+1, y-1);  
+        }
+              //mark it's set as checked
+              //uncover this square and check the neighbors
     },
 
-    findNeighbors : function(x,y){
-        var left         = [x     , y - 1]
-        var upLeft       = [x - 1 , y - 1]
-        var up           = [x - 1 , y    ]
-        var upRight      = [x - 1 , y + 1]
-        var right        = [x     , y + 1]
-        var downRight    = [x + 1 , y + 1]
-        var down         = [x + 1 , y    ]
-        var downLeft     = [x + 1 , y - 1]
-        var neighbors   = [left, upLeft, up, upRight, right, downRight,down, downLeft]
-      return neighbors
+    updateTileHTML : function(x,y){
+      var tile = board.tiles[x][y];
+      var tileHtml = $("#"+x+""+y);
+      if(tile.isCovered == false){
+          tileHtml.removeClass('covered');
+      }
+      
+      tileHtml.html(tile.value);
     }
+  };
 
 
-
-
-    
-    
-
-  }
- board.render()
- console.log(board.values)
-console.log(board.bombSet(board.numRows))
-console.log(board.bombs)
-board.update();
-board.bombCheck();
+ board.render();
 
 
 
   var game = {
     turn: function(){
-      board.destroy();
       board.createRows(board.numRows);
     }
-  }
+  };
 /**
 Board Object
 
